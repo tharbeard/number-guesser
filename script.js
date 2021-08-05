989afa6e301bcb13fa3c3d956357d40ee120f2b0
Trevor Harbeard <39714067+tharbeard@users.noreply.github.com>
2021-08-04 22:11:50 -0700

    Update script.js
    Added comments.

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-let humanScore = 0;
-let computerScore = 0;
-let currentRoundNumber = 1;
+let humanScore = 0; //tracks human player score
+let computerScore = 0; //tracks computer player score
+let currentRoundNumber = 1; //tracks number of rounds
 
-// Write your code below:
+// generate a target number
+const generateTarget = () => Math.floor(Math.random() * 10);
 
+//determine winner
+const compareGuesses = (userGuess, computerGuess, target) => {
+  let diffUser = getAbsoluteDistance(target, userGuess);
+  let diffComputer = getAbsoluteDistance(target, computerGuess);
+  if (diffUser === diffComputer) {
+    return true;
+  } else if (diffUser < diffComputer) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+//gets absolute value for detemining winner
+const getAbsoluteDistance = (num1, num2) => Math.abs(num1 - num2);
+
+//updates score
+const updateScore = winner => (winner === 'human') ? humanScore += 1 : computerScore += 1;
+
+//updates round
+const advanceRound = () => currentRoundNumber += 1;
